8399f7eed55c9670a6d23d1e891544e94f1eeb68
CesarSanchez19 <2302073@utrivieramaya.edu.mx>
2025-01-22 07:28:13 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ti-41-blank',
+  webDir: 'www'
+};
+
+export default config;
