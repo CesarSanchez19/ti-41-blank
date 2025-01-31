0905f281c1100e079d443e05f9ae7460df22c888
CesarSanchez19 <2302073@utrivieramaya.edu.mx>
2025-01-30 19:47:41 -0500

    pruebas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { HomePage } from './home/home.page'
+import { UserComponent } from './components/user/user.component'
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CalcComponent } from './components/calc/calc.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomePage, UserComponent, CalcComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss'],
+  standalone: false,
+})
+export class UserComponent  implements OnInit {
+
+  public title: string = 'User Component';
+  // Declaramos el dato de entrada de tipo string
+  public calcTitle: string = 'Calc component'
+  public isAdmin: boolean = false;
+  public isAlertOpen: boolean = false;
+  public alertButtons: string[] = ['Yes','No'];
+  public alertMessage: string = '';
+
+  constructor() { }
+
+  ngOnInit () {}
+
+  toggleAdmin() {
+    this.isAdmin = !this.isAdmin;
+  }
+
+  handleEvent(event: string) {
+    this.alertMessage = event;
+    this.isAlertOpen = !this.isAlertOpen;
+    alert(event);
+  }
+
+}
