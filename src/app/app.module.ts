35b6fa74fbe71997e843105fc088d58ecbddccf9
CesarSanchez19 <2302073@utrivieramaya.edu.mx>
2025-02-05 08:45:24 -0500

    router y forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,31 @@ import { TaxPipe } from './pipes/tax.pipe'
 import { HighlightDirective } from './highlight.directive'
 
 import { HomePage } from './home/home.page'
+import { LoginComponent } from './components/login/login.component'
 import { UserComponent } from './components/user/user.component'
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CalcComponent } from './components/calc/calc.component';
+import {ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
 
   // Importar y declara los modulos
-  declarations: [AppComponent, HomePage, UserComponent, CalcComponent,],
+  declarations: [AppComponent,
+    HomePage,
+    UserComponent,
+    CalcComponent,
+    LoginComponent, ],
   // Importar los fragmentos de codigo de manera glogal
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, TaxPipe ,HighlightDirective],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    TaxPipe ,
+    HighlightDirective,
+    ReactiveFormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
