3e0afe68ba7d3b87d0c781617a92c79711b0e0a9
CesarSanchez19 <2302073@utrivieramaya.edu.mx>
2025-02-28 08:43:46 -0500

    fix(app.module.ts, http.service.ts, login.components.ts, http.service.spec.ts): conexion al frontend al backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,43 @@
-// Este es nucleo de importancion de este proyecto
-
-// Angular Core
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-
-// Ionic
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
-// Components
-import { AppComponent } from './app.component';
-import { HomePage } from './home/home.page';
-import { LoginComponent } from './components/login/login.component';
-import { UserComponent } from './components/user/user.component';
-import { CalcComponent } from './components/calc/calc.component';
-
-// Services
 import { LocalStorageService } from './services/local-storage.service';
+import { HttpService } from './services/http.service';
 
-// Pipes and Directives
 import { TaxPipe } from './pipes/tax.pipe';
 import { HighlightDirective } from './highlight.directive';
 
-// Modules
-import { AppRoutingModule } from './app-routing.module';
+import { HomePage } from "./home/home.page";
 
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { UserComponent } from './components/user/user.component'
+import { CalcComponent } from './components/calc/calc.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-
-  // Importar y declara los modulos
-  declarations: [
-    AppComponent,
+  declarations: [AppComponent,
     HomePage,
     UserComponent,
     CalcComponent,
-    LoginComponent ],
-  // Importar los fragmentos de codigo de manera glogal
+    LoginComponent],
+
   imports: [BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    TaxPipe ,
-    HighlightDirective,
-    ReactiveFormsModule],
-  providers: [
+    ReactiveFormsModule,
+    TaxPipe,
+    HighlightDirective],
 
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  providers: [{ provide: RouteReuseStrategy,
+    useClass: IonicRouteStrategy },
     LocalStorageService,
+    HttpService
   ],
   bootstrap: [AppComponent],
 })
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,38 +1,56 @@
+import { LocalStorageService } from '../../services/local-storage.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
-import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  standalone: false
+  standalone: false,
 })
 export class LoginComponent  implements OnInit {
 
-  constructor(private router: Router,
-    private localStorageService: LocalStorageService
+  constructor(
+    private router: Router,
+    private localStorageService: LocalStorageService,
+    private httpService: HttpService
   ) { }
 
   public user = new FormControl('');
   public password = new FormControl('');
 
   ngOnInit() {
-    this.localStorageService.setItem('logedIn', false)
+    this.localStorageService.setItem('logedIn', false);
   }
 
   login() {
-    console.log('User: ', this.user.value);
-    console.log('Password:' , this.password.value);
+    const payload = {
+      condition: {
+        name: this.user.value,
+        password: this.password.value
+      }
+    };
 
-    if(this.user.value === 'admin' && this.password.value === 'Admin123') {
-      this.localStorageService.setItem('logedIn', true)
-      this.router.navigate(['/home']); //Descomentar si quieres que redirija al home
-    } else {
-      alert('User or password incorrect');
-    }
-    // return;
+    this.httpService.post('get-user', payload).subscribe((response: any) => {
+      console.log('Response from server', response);
+      if(response.ok){
+        this.localStorageService.setItem('logedIn', true);
+        this.router.navigate(['/home']);
+      } else {
+        alert(response.message);
+      }
+    })
+
+    // console.log('User: ', this.user.value);
+    // console.log('Password: ', this.password.value);
+    // if(this.user.value === 'admin' && this.password.value === 'admin123') {
+    //   this.localStorageService.setItem('logedIn', true);
+    //   this.router.navigate(['/home']);
+    // } else {
+    //   alert('User or password incorrect');
+    // }
   }
 
 }
